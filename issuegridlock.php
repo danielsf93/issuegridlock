bae77fb7c18c5b8f6e350c0bbc56fc51322597e2
Daniel Silva Ferreira <danielsilva@usp.br>
2023-03-06 11:38:52 -0300

    Add files via upload

--- issuegridlock.php
+++ issuegridlock.php
@@ -5,13 +5,22 @@ import('lib.pkp.classes.plugins.GenericPlugin');
 class issuegridlock extends GenericPlugin {
     public function register($category, $path, $mainContextId = NULL) {
         $success = parent::register($category, $path);
-          if ($success && $this->getEnabled()) {
-    HookRegistry::register('TemplateResource::getFilename', array($this, '_overridePluginTemplates'));
-    HookRegistry::register('OrderItemsFeature::_saveRowsInCategoriesSequence', array(&$this, '_saveRowsInCategoriesSequence'));
-}
+            if ($success && $this->getEnabled()) {
+               HookRegistry::register('TemplateResource::getFilename', array($this, '_overridePluginTemplates'));
+               HookRegistry::register('TemplateResource::getFilename', array($this, '_overrideOrderCategoryGridItemsFeature'));
 
+            }
         return $success;
     }
+
+
+public function _overrideOrderCategoryGridItemsFeature($hookName, $args) {
+    $templatePath = $args[0];
+    if ($templatePath === 'classes/controllers/grid/feature/OrderCategoryGridItemsFeature.inc.php') {
+        $args[0] = 'plugins/generic/issuegridlock/OrderCategoryGridItemsFeature.inc.php';
+    }
+    return false;
+}
 
   /**
    * Provide a name for this plugin
@@ -41,60 +50,6 @@ class issuegridlock extends GenericPlugin {
 	function getContextSpecificPluginSettingsFile() {
 		return $this->getPluginPath() . '/settings.xml';
 	}
-
-
-
-
-
-
-public function saveSequence($args) {
-		$request =& $args['request'];
-		$grid =& $args['grid'];
-
-		$data = json_decode($request->getUserVar('data'));
-		$gridCategoryElements = $grid->getGridDataElements($request);
-
-		if ($this->getType() != ORDER_CATEGORY_GRID_CATEGORIES_ROWS_ONLY) {
-			$categoriesData = array();
-			foreach($data as $categoryData) {
-				$categoriesData[] = $categoryData->categoryId;
-			}
-
-			
-		}
-
-		// Save rows sequence, if this grid has also orderable rows inside each category.
-		$this->_saveRowsInCategoriesSequence($request, $grid, $gridCategoryElements, $data);
-	}
-
- 
-	public function _saveRowsInCategoriesSequence($request, &$grid, $gridCategoryElements, $data) {
-		if ($this->getType() != ORDER_CATEGORY_GRID_CATEGORIES_ONLY) {
-			foreach($gridCategoryElements as $categoryId => $element) {
-				$gridRowElements = $grid->getGridCategoryDataElements($request, $element);
-				if (!$gridRowElements) continue;
-
-				// Get the correct rows sequence data.
-				$rowsData = null;
-				foreach ($data as $categoryData) {
-					if ($categoryData->categoryId == $categoryId) {
-						$rowsData = $categoryData->rowsId;
-						break;
-					}
-				}
-
-				unset($rowsData[0]); // remove the first element, it is always the parent category ID
-				foreach ($gridRowElements as $rowId => $element) {
-					$newSequence = array_search($rowId, $rowsData);
-					$currentSequence = $grid->getDataElementInCategorySequence($categoryId, $element);
-					if ($newSequence != $currentSequence) {
-						$grid->setDataElementInCategorySequence($categoryId, $element, $newSequence);
-					}
-				}
-			}
-		}
-	}
-
 
 	
 }
